1f5f68200c526da7a1c19946bffe0b2197239530
paulinebennett <paulinebennett96@gmail.com>
2017-03-25 17:38:58 -0400

    added pickup/dropoff page for drivers

---
+++ includes/pickup-dropoff.php
@@ -0,0 +1,16 @@
+<h2>Pick up / Drop off</h2>
+
+<!--
+Same form
+-->
+
+<input>
+    Dropoff or Pickup?
+    <input type="radio" value="dropoff">Dropoff <br>
+    <input type="radio" value="pickup">Pickup <br>
+
+
+
+
+    <input type="submit" value="Submit">
+</form>
