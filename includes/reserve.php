2edee7b6cd4425b9e04ecd9f7f7e7dfc7f4e2b1c
paulinebennett <paulinebennett96@gmail.com>
2017-03-25 17:28:27 -0400

    added reserve (default page upon logging in for non-admin users)

---
+++ includes/reserve.php
@@ -0,0 +1,8 @@
+<h1>Welcome!</h1>
+    <h2>Reserve</h2>
+    <div id="links">
+        <!-- make these link to pages -->
+        Pick up/Drop off<br>
+        Rental history<br>
+        Feedback<br>
+    </div>
