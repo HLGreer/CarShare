f7b1a96dfdb3691619aea7edc4c4084be49a6cd2
hlgreer <hannahgreer@rogers.com>
2017-04-02 18:54:34 -0400

    Sign up form works

--- public_html/signup-verify.php
+++ public_html/signup-verify.php
@@ -11,27 +11,21 @@ if (!isset($_SESSION['memberID'])) {
         $db = getDB();
         //$hash_password= hash('sha256', $dbpass); //Password encryption
         $query = $db->prepare("INSERT INTO $tbl_name (password_hash, address, firstName, lastName, email, phoneNum, driverLicence, membershipFee) VALUES (:password, :address, :firstName, :lastName, :email, :phoneNum, :DLicense, 34);");
-        $query->bindParam("email", $_POST['Email'], PDO::PARAM_STR);
-        $query->bindParam("password", $_POST['Password'], PDO::PARAM_STR);
-        $query->bindParam("address", $_POST['Address'], PDO::PARAM_STR);
-        $query->bindParam("firstName", $_POST['Firstname'], PDO::PARAM_STR);
-        $query->bindParam("lastName", $_POST['Lastname'], PDO::PARAM_STR);
+        $query->bindParam("email", $_POST['email'], PDO::PARAM_STR);
+        $query->bindParam("password", $_POST['password'], PDO::PARAM_STR);
+        $query->bindParam("address", $_POST['address'], PDO::PARAM_STR);
+        $query->bindParam("firstName", $_POST['first'], PDO::PARAM_STR);
+        $query->bindParam("lastName", $_POST['last'], PDO::PARAM_STR);
         $query->bindParam("phoneNum", $_POST['phoneNum'], PDO::PARAM_STR);
         $query->bindParam("DLicense", $_POST['licenseNum'], PDO::PARAM_STR);
         //$query->bindParam("fee", 34, PDO::PARAM_STR);
-        $query->execute();
-
-        $count = $query->rowCount();
-        $data = $query->fetch(PDO::FETCH_OBJ);
-        $db = null;
-        if ($count == 1) {// there should only be one matching entry
-            echo "Hello";
-            //$_SESSION['memberID'] = $data->memberID; // Storing user session value
-            //echo $_SESSION['memberID'];
-            //header('Location: memberHome.php');
+        $result = $query->execute();
+        if ($result) {
+            header('Location: login.php');
         } else {
-            header('Location: login.php'); // TODO: We should add an error message if redirect to login.php
+            header('Location: signup.php');
         }
+
     } catch (PDOException $e) {
         echo '{"error":{"text":' . $e->getMessage() . '}}';
     }
