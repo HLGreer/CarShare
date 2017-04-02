20b5abf9ee3ba5051d3361448e201e5299c750f2
hlgreer <hannahgreer@rogers.com>
2017-04-02 16:30:16 -0400

    Login, logout currently working, and access to the correct pages according to your user session also working.

--- public_html/signup-verify.php
+++ public_html/signup-verify.php
@@ -1,4 +1,34 @@
 <?php
+require("../includes/config.php");
 /**
  *
  */
+
+$tbl_name = "member";
+if ((isset($_POST['email'])) & (isset($_POST['password']))) {
+
+    try {
+        $db = getDB();
+        //$hash_password= hash('sha256', $dbpass); //Password encryption
+        $query = $db->prepare("INSERT INTO $tbl_name (password_hash, email, age, )");
+        $query->bindParam("email", $_POST['email'], PDO::PARAM_STR);
+        $query->bindParam("password", $_POST['password'], PDO::PARAM_STR);
+        $query->execute();
+        $count = $query->rowCount();
+        $data = $query->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        if ($count == 1) {// there should only be one matching entry
+            echo "Hello";
+            $_SESSION['memberID'] = $data->memberID; // Storing user session value
+            echo $_SESSION['memberID'];
+            //header('Location: memberHome.php');
+        } else {
+            header('Location: login.php'); // TODO: We should add an error message if redirect to login.php
+        }
+    } catch (PDOException $e) {
+        echo '{"error":{"text":' . $e->getMessage() . '}}';
+    }
+} else {
+    header('Location: login.php');
+    echo "hello";
+}
