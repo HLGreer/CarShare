80a93ba93427b54dfc4c1a7181e1fcd3424f5bb6
paulinebennett <paulinebennett96@gmail.com>
2017-04-02 15:03:52 -0400

    added "Add car to fleet" page for admins

---
+++ templates/add-car.php
@@ -0,0 +1,19 @@
+
+<h2>Add Car to Fleet</h2>
+
+<!--
+Same form
+-->
+<form>
+    <label for="make">Make:</label>
+        <input type="text" name="make"><br>
+    <label for="model">Model:</label>
+        <input type="text" name="model"><br>
+    <label for="number">Year:</label>
+        <input type="number" name="year">
+    <label for="vin">VIN:</label>
+        <input type="number" name="vin">
+    <label for="fee">Rental Fee:</label>
+        <input type="number" name="fee">
+    <input type="submit" value="Submit">
+</form>
--- templates/pickup-dropoff.php
+++ templates/pickup-dropoff.php
@@ -1,25 +1,25 @@
-<h<h2>Pick up / Drop off</h2>
+<h2>Pick up / Drop off</h2>
 
 <!--
 Same form
 -->
-
-<input>
-Dropoff or Pickup? <br>
-<input type="radio" name="pord" value="dropoff">Dropoff <br>
-<input type="radio" name="pord" value="pickup">Pickup <br>
-Reservation ID: <input type="text" name="id"> <br>
-<br>
-Date and time? <br>
-<input type="datetime-local" name="usrtime"> <br>
-<br>
-Odometer Reading? (how many kms) <br>
-<input type="number" min="0" max="1000000"<br>
-<br>
-Car Status? <br>
-<input type ="radio" name="carstatus" value="normal">Normal <br>
-<input type ="radio" name="carstatus" value="damaged">Damaged <br>
-<input type ="radio" name="carstatus" value="notRunning">Not running <br>
-<br>
-<input type="submit" value="Submit">
+<form>
+    <input>
+    Dropoff or Pickup? <br>
+    <input type="radio" name="pord" value="dropoff">Dropoff <br>
+    <input type="radio" name="pord" value="pickup">Pickup <br>
+    Reservation ID: <input type="text" name="id"> <br>
+    <br>
+    Date and time? <br>
+    <input type="datetime-local" name="usrtime"> <br>
+    <br>
+    Odometer Reading? (how many kms) <br>
+    <input type="number" min="0" max="1000000"<br>
+    <br>
+    Car Status? <br>
+    <input type ="radio" name="carstatus" value="normal">Normal <br>
+    <input type ="radio" name="carstatus" value="damaged">Damaged <br>
+    <input type ="radio" name="carstatus" value="notRunning">Not running <br>
+    <br>
+    <input type="submit" value="Submit">
 </form>
