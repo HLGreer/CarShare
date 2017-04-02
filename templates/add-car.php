896b1609e5dfd1859e37169c45a0f1b457e70ae8
paulinebennett <paulinebennett96@gmail.com>
2017-04-02 17:23:36 -0400

    fixed add-car form format

--- templates/add-car.php
+++ templates/add-car.php
@@ -10,10 +10,10 @@ Admin only
     <label for="model">Model:</label>
         <input type="text" name="model"><br>
     <label for="number">Year:</label>
-        <input type="number" name="year">
+        <input type="number" name="year"><br>
     <label for="vin">VIN:</label>
-        <input type="number" name="vin">
+        <input type="number" name="vin"><br>
     <label for="fee">Rental Fee:</label>
-        <input type="number" name="fee">
+        <input type="number" name="fee"><br>
     <input type="submit" value="Submit">
 </form>
