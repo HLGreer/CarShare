b466b4544bb9390699b4a2ed45575d0b26810eab
hlgreer <hannahgreer@rogers.com>
2017-04-04 10:10:25 -0400

    Styling home page

--- templates/home.php
+++ templates/home.php
@@ -1,17 +1,49 @@
-<h1> Hello.</h1>
-<h3>These are our locations:</h3><br>
+
+
+<div class="container-fluid">
+    <div class="jumbotron" id="firstJum">
+        <p>Get on the Road, without being tied down.</p>
+    </div>
+    <div class="jumbotron">
+        <h3>These are our locations:</h3><br>
+        <table class="table text-center">
+            <tr>
+                <td>LOT 1</td>
+                <td>238 Victoria Street</td>
+            </tr>
+            <tr>
+                <td>LOT 2</td>
+                <td>42 Amazing Street</td>
+            </tr>
+            <tr>
+                <td>LOT 3</td>
+                <td>25 Union Street</td>
+            </tr>
+        </table>
+    </div class="jumbotron">
+
+    <div class="col-lg-6"><img style="width: 100%; height: 90%;" src="../locations.png"/></div>
+    <div class=""col-lg-6">
+    <p>
+      With
+    </p>
+    </div>
+
+
+
 <table class="table">
     <tr>
         <td>LOT 1</td>
-        <td>238 Victoria street</td>
+        <td>238 Victoria Street</td>
     </tr>
     <tr>
         <td>LOT 2</td>
-        <td>42 Amazing street</td>
+        <td>42 Amazing Street</td>
     </tr>
     <tr>
         <td>LOT 3</td>
-        <td>25 Union street</td>
+        <td>25 Union Street</td>
     </tr>
 </table>
-<img src="../locations.png"><br><br>
+
+</div>
