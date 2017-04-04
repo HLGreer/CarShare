6c6461b13adcacdd541695114d20b780008559d2
DominiqueSteneker <13dls6@queensu.ca>
2017-04-04 12:16:56 -0400

    slightly changed sql query for highest and lowest rentals

--- public_html/adminHome.php
+++ public_html/adminHome.php
@@ -9,7 +9,11 @@ try {
     //kms
     $conn = getDB();
     //unique VIN row for most recent odometer reading
-    $stmt = $conn->prepare("SELECT VIN, make, model, year, COUNT(reservationNum) as numRentals FROM rentalhistory NATURAL RIGHT OUTER JOIN car GROUP BY VIN HAVING MIN(numRentals) = numRentals OR MAX(numRentals) = numRentals");
+    $stmt = $conn->prepare("SELECT *
+FROM( SELECT VIN, make, model, year, COUNT(reservationNum) as numRentals 
+FROM rentalhistory NATURAL RIGHT OUTER JOIN car 
+GROUP BY VIN ) temp1
+HAVING MIN(numRentals) OR MAX(numRentals);");
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_OBJ);
     $header = array("VIN", "Make","Model","Year","Number of Reservations");
