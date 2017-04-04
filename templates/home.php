84fd0f78d783bc66ef5c09347e387f0f0b012ff2
hlgreer <hannahgreer@rogers.com>
2017-04-04 10:30:02 -0400

    Styling home page

--- templates/home.php
+++ templates/home.php
@@ -5,7 +5,7 @@
         <p>Get on the Road, without being tied down.</p>
     </div>
     <div class="jumbotron">
-        <h3>These are our locations:</h3><br>
+        <h3>Locations Available:</h3><br>
         <table class="table text-center">
             <tr>
                 <td>LOT 1</td>
@@ -20,30 +20,6 @@
                 <td>25 Union Street</td>
             </tr>
         </table>
-    </div class="jumbotron">
-
-    <div class="col-lg-6"><img style="width: 100%; height: 90%;" src="../locations.png"/></div>
-    <div class=""col-lg-6">
-    <p>
-      With
-    </p>
     </div>
-
-
-
-<table class="table">
-    <tr>
-        <td>LOT 1</td>
-        <td>238 Victoria Street</td>
-    </tr>
-    <tr>
-        <td>LOT 2</td>
-        <td>42 Amazing Street</td>
-    </tr>
-    <tr>
-        <td>LOT 3</td>
-        <td>25 Union Street</td>
-    </tr>
-</table>
-
+    <div class="jumbotron" id="secondJum"></div>
 </div>
