0558514cab16153e1e87e79b4e6713930208c7c8
paulinebennett <paulinebennett96@gmail.com>
2017-04-04 01:55:45 -0400

    pickup-dropoff all done!!!

--- templates/header.php
+++ templates/header.php
@@ -29,6 +29,7 @@
                 echo '<ul class="nav navbar-nav">
                         <li><a href="../public_html/memberRental-history.php">Rental History</a></li>
                         <li><a href="../templates/feedback.php">Feedback</a></li>
+                        <li><a href="../templates/pickup-dropoff.php">Pick up/Drop off Car</a></li>
                     </ul>';
             }
             elseif (isset($_SESSION['user_type']) &&  $_SESSION['user_type']== "admin"){
--- templates/pickup-dropoff.php
+++ templates/pickup-dropoff.php
@@ -4,22 +4,22 @@
 <!--
 Should only show form if member has made a reservation!
 -->
-<form>
+<form action="../public_html/pickup-dropoff-verify.php" method="post">
+    Reservation Number<br>
+    <input type="number" name="reservationNUM"><br>
     Dropoff or Pickup? <br>
     <input type="radio" name="pord" value="dropoff">Dropoff <br>
     <input type="radio" name="pord" value="pickup">Pickup <br>
-    Reservation ID: <input type="text" name="id"> <br>
-    <br>
-    Date and time? <br>
-    <input type="datetime-local" name="usrtime"> <br>
+    Date? <br>
+    <input type="date" name="d"> <br>
     <br>
     Odometer Reading? (how many kms) <br>
-    <input type="number" min="0" max="1000000"<br>
+    <input type="number" min="0" max="1000000" name="odometer"><br>
     <br>
-    Car Status? <br>
+    If dropoff, what is the car's status? <br>
     <input type ="radio" name="carstatus" value="normal">Normal <br>
     <input type ="radio" name="carstatus" value="damaged">Damaged <br>
-    <input type ="radio" name="carstatus" value="notRunning">Not running <br>
+    <input type ="radio" name="carstatus" value="not running">Not running <br>
     <br>
     <input type="submit" value="Submit">
 </form>
