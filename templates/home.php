e48814b520f4215762c89d20b81b0076750e807a
paulinebennett <paulinebennett96@gmail.com>
2017-04-04 03:08:42 -0400

    home page doneish

--- templates/home.php
+++ templates/home.php
@@ -1,9 +1,17 @@
 <h1> Hello.</h1>
-<div>
-    <img src="https://d13yacurqjgara.cloudfront.net/users/209188/screenshots/1234644/fiat_donut.gif">
-</div>
-
-
-<div>
-    <h2></h2>
-</div>
+<h3>These are our locations:</h3><br>
+<table class="table">
+    <tr>
+        <td>LOT 1</td>
+        <td>238 Victoria street</td>
+    </tr>
+    <tr>
+        <td>LOT 2</td>
+        <td>42 Amazing street</td>
+    </tr>
+    <tr>
+        <td>LOT 3</td>
+        <td>25 Union street</td>
+    </tr>
+</table>
+<img src="../locations.png"><br><br>
