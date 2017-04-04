b79dd4780f58a00ff461f2998195849f4607579d
hlgreer <hannahgreer@rogers.com>
2017-04-04 03:56:34 -0400

    Admin page to view all available cars for the current day is done, this was a bit of a hack.

--- public_html/adminLocationAvail.php
+++ public_html/adminLocationAvail.php
@@ -6,21 +6,17 @@ require("../includes/config.php");
  * the View page with the appropriate information in values.
  */
 
-
-// Print the array from getdate()
-//print_r(getdate());
-//echo "<br><br>";
-
 // Return date/time info of a timestamp; then format the output
-$mydate=getdate(date("U"));
-print_r(getdate(date("U")));
+
 //echo "$mydate[weekday], $mydate[month] $mydate[mday], $mydate[year]";
 
 try {
+    $mydate=getdate(date("U"));
     $db = getDB();
     //$hash_password= hash('sha256', $dbpass); //Password encryption
     $query = $db->prepare("SELECT * FROM car WHERE VIN NOT IN (SELECT VIN FROM reservation WHERE ((pickUpDate <= :resDate AND dropOffDate >= :resDate) OR (pickUpDate = :resDate) OR (dropOffDate = :resDate)));");
     $query->bindParam("resDate", $mydate, PDO::PARAM_STR);
+
     //$query->bindParam("lot", $_POST['lot'], PDO::PARAM_STR);
     $query->execute();
     //echo "hello" . "<br>";
@@ -31,7 +27,6 @@ try {
     $db = null;
     if ($count) {// there should only be one matching entry
         $values['title'] = "Available Cars Today";
-        $values['mydate'] = $mydate;
         $values['data'] = $data;
         render("../templates/adminLocationAvail-view.php", $values, __FILE__);
 
@@ -42,7 +37,6 @@ try {
         <p>"All cars are rented today";</p>
         </div>
         <?php
-        //header('Location: login.php'); // TODO: We should add an error message if redirect to login.php
     }
 } catch (PDOException $e) {
     echo '{"error":{"text":' . $e->getMessage() . '}}';
